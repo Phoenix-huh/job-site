b86cde7a5ee234835df5920d4f917fbeb78f85a3
Your Name <your@email.com>
2026-07-26 18:31:54 +0530

    make root blank

--- api/index.py
+++ api/index.py
@@ -2,10 +2,9 @@ import sys
 import os
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-
 backend_dir = os.path.abspath(os.path.join(current_dir, "..", "backend"))
-if os.path.isdir(backend_dir):
-    if backend_dir not in sys.path:
-        sys.path.insert(0, backend_dir)
+
+if backend_dir not in sys.path:
+    sys.path.insert(0, backend_dir)
 
 from main import app
